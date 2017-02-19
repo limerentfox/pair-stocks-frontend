fde9001dacb430349130260f3e4ce9f4e7b9efa1
Mohammed Chisti <Chisti@Mohammeds-Air.home>
2017-02-18 21:59:43 -0500

    Added user profile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,6 @@ export default class NavBar extends Component {
    return (
      <Menu secondary>
        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-       <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-       <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
        <Menu.Menu position='right'>
          <Menu.Item>
            <Input icon='search' placeholder='Search...' />
