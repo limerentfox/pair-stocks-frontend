994c2cd121162979cda9d065fd41f6085dac41df
limerentfox <christi@limerentfox.com>
2017-02-20 16:12:26 -0500

    shadows

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
 import { followStock } from '../actions'
-import { Icon } from 'semantic-ui-react'
+import { Icon, Button, Header } from 'semantic-ui-react'
 
 class SearchResults extends Component {
   constructor() {
@@ -34,14 +34,30 @@ class SearchResults extends Component {
       <div className="results">
 
         <div className="padded" onClick={ this.handleResultClick }>
-          <p>{`${ this.props.ticker }: ${ this.props.company_name }`}</p>
-          <Icon link size='large' name='add circle' color='green' onClick={ this.handleFollow } />
+          <p className="stock-header" >{`${ this.props.ticker }: ${ this.props.company_name }`}</p>
+
+          <Button className='follow-button' size='mini' color='green' onClick={ this.handleFollow } animated='fade'>
+            <Button.Content  visible>
+              <Icon name='add'/>
+
+            </Button.Content>
+            <Button.Content hidden>
+              Follow
+            </Button.Content>
+
+
+          </Button>
         </div>
 
 
 
 
         </div>
+
+
+
+
+
 
     )
   }
