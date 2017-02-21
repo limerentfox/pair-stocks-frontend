f7ce58d70255707fbb63c0e37f398e114dd83102
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-02-20 22:46:57 -0500

    User card changed

--- src/reducers/stockReducer.js
+++ src/reducers/stockReducer.js
@@ -1,6 +1,7 @@
 export default function(state=[], action){
   switch (action.type) {
     case 'FETCH_STOCKS_OWNED':
+      debugger
       return action.payload
     case 'ADD_STOCK':
       return [...state, action.payload]
