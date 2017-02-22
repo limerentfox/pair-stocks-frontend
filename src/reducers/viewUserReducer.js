c0fd61b581bd4ea5754cbe4295144bbb9f6a5401
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-02-22 14:01:46 -0500

    Search works with users

--- src/reducers/viewUserReducer.js
+++ src/reducers/viewUserReducer.js
@@ -1,6 +1,7 @@
 export default function viewUserReducer( state=[], action ){
   switch (action.type) {
     case 'FETCH_OTHER_USER':
+      debugger
       return action.payload.data
     default:
       return state
