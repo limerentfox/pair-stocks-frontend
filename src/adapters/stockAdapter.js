9c65273e360cd71c063294c9aa6b7024f9b425ae
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-03-16 15:46:09 -0400

    changed to heroku api

--- src/adapters/stockAdapter.js
+++ src/adapters/stockAdapter.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:4000/api/v1/'
+axios.defaults.baseURL = 'http://pair-social-stocks-api.herokuapp.com/api/v1/'
 axios.defaults.headers.common['AUTHORIZATION'] = sessionStorage.getItem('jwt')
 
 export const stockAdapter = {
